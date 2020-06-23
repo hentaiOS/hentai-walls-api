2dd65f0c9143ab442c14704fc8969a348cef2d56
ATechnoHazard <amolele@gmail.com>
2020-06-23 22:31:21 +0530

    logging: Add request ID tracing middleware
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express from 'express'
 import compression from 'compression'
 import cors from 'cors'
+import rTracer from 'cls-rtracer'
+
 import morgan from './logging/morgan'
 import index from './routes/index'
 
 const app = express()
 
 // Middleware chain
+app.use(rTracer.expressMiddleware({
+  useHeader: true,
+  headerName: 'X-Request-Id'
+}))
 app.use(express.json())
 app.use(compression())
 app.use(cors())
--- src/logging/logger.ts
+++ src/logging/logger.ts
@@ -1,13 +1,20 @@
-import winston from 'winston'
+import { createLogger, format, transports } from 'winston'
+import rTracer from 'cls-rtracer'
 
-const logger = winston.createLogger({
+const rTracerFormat = format((info) => {
+  const rid = rTracer.id()
+  info.message = rid ? `[request-id: ${rid}]: ${info.message}` : info.message
+  return info
+})
+
+const logger = createLogger({
   level: 'silly',
-  format: winston.format.json(),
+  format: format.combine(rTracerFormat(), format.simple()),
   transports: [
-    new winston.transports.Console({
+    new transports.Console({
       level: 'silly',
       handleExceptions: true,
-      format: winston.format.combine(winston.format.colorize(), winston.format.simple())
+      format: format.combine(format.colorize(), format.simple())
     })
   ]
 })
